b9502a0f9d5d0015dd77fd313dfce387a4061330
Ramiro Otero <ramirootero98@gmail.com>
2025-07-09 23:10:04 -0300

    Starting build login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Que bueno que haz vuelto</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Porfavor, ingrese sus datos para ingresar
+        </p>
+      </div>
+    </AuthLayout>
   )
 }
 
